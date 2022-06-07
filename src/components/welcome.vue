399fb81eed0bf81fc519c7ec156579c33a4f00b8
yinyue <664960698@qq.com>
2022-06-07 22:31:52 +0800

    edit

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -22,6 +22,34 @@
         :style="{ opacity: isShowPic ? 0.2 : 0 }"
       />
     </el-col>
+    <el-col :span="6" style="justify-content: flex-end; padding-right: 20px">
+      <a href="https://github.com/yyISACoder" target="_blank" rel="noreferrer">
+        <svg
+          height="32"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          version="1.1"
+          width="32"
+          data-view-component="true"
+          class="octicon octicon-mark-github v-align-middle"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+          ></path>
+        </svg>
+      </a>
+      <a
+        href="https://juejin.cn/user/712139266332670"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img :src="juejin" class="nav-logo" />
+      </a>
+      <a href="https://www.npmjs.com/~yinyue" target="_blank" rel="noreferrer">
+        <img :src="npm" class="nav-logo" />
+      </a>
+    </el-col>
   </el-row>
   <div v-loading="isLoadingNews" class="nav-container">
     <el-row :gutter="12" class="card-row">
@@ -313,6 +341,7 @@ import juejin from '../assets/images/juejin.jpeg'
 import tx from '../assets/images/tx.jpeg'
 import ysw from '../assets/images/ysw.jpeg'
 import nba from '../assets/images/nba.jpeg'
+import npm from '../assets/images/npm.jpeg'
 import { apiKey } from '../utils/const'
 
 const searchKey = ref('')
@@ -525,6 +554,12 @@ onMounted(() => {
   .logo {
     height: 28px;
     margin: 0 6px 0 20px;
+  }
+  .nav-logo {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    margin-left: 12px;
   }
   .title {
     color: #4a4a4a;
